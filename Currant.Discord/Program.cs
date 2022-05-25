b96f6e446b526cc9d1301090752a81c86d93ceb5
Alex <69281817+juuuuce@users.noreply.github.com>
2022-05-25 17:56:53 +0100

    initial commit

---
+++ Currant.Discord/Program.cs
@@ -0,0 +1,36 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Logging;
+
+namespace Currant;
+
+public class Currant
+{
+    public Currant(CommandsNextExtension commands)
+    {
+        Commands = commands;
+    }
+
+    public static DiscordClient Client { get; set; } = null!;
+    public CommandsNextExtension Commands { get; set; }
+
+    private static void Main()
+    {
+        MainAsync().GetAwaiter().GetResult();
+    }
+
+    private static async Task MainAsync()
+    {
+        var discordConfig = new DiscordConfiguration()
+        {
+            Token = "",
+            TokenType = TokenType.Bot,
+            AutoReconnect = true,
+            MinimumLogLevel = LogLevel.Debug,
+            Intents = DiscordIntents.AllUnprivileged,
+        };
+        
+        await Client.ConnectAsync();
+        await Task.Delay(-1);
+    }
+}
